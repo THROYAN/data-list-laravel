85b5d2e30ba3a3635dd38dd9eeaf09e188376e87
Oleh <throyan@gmail.com>
2023-03-10 15:37:13 +0100

    Prettify house list

--- frontend/src/components/HouseFilter.vue
+++ frontend/src/components/HouseFilter.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-form v-if="selected" ref="filter-form" :model="selected">
+    <el-form v-if="selected" ref="filter-form" :model="selected" label-position="top" size="mini">
         <el-form-item label="Name">
             <el-input placeholder="Name" :clearable="true" v-model="selected.name"></el-input>
         </el-form-item>
@@ -15,11 +15,11 @@
         <el-form-item label="Garages">
             <el-input type="number" placeholder="Garages" size="small" :min="0" v-model="selected.garageCount"/>
         </el-form-item>
-        <el-form-item label="Price" label-position="top">
+        <el-form-item label="Price">
             <el-slider
                 v-model="selected.price"
                 range
-                :max="1000000"
+                :max="maxPrice"
                 label="Price"
             />
         </el-form-item>
@@ -40,10 +40,8 @@
         data() {
             return {
                 selected: this.value,
+                maxPrice: 100000,
             }
-        },
-        mounted() {
-            console.log(this.selected)
         },
         methods: {
             onClear() {
@@ -55,15 +53,20 @@
                 this.selected.bathroomCount = null;
                 this.selected.storeyCount = null;
                 this.selected.garageCount = null;
-                this.selected.price = [];
+                this.selected.price = [0, this.maxPrice];
 
                 this.onSubmit();
             },
             onSubmit() {
-                console.log(this.selected);
                 this.$emit('input', this.selected)
                 this.$emit('onChange', this.selected)
             }
         }
     })
 </script>
+
+<style scoped>
+form {
+    margin: 20px;
+}
+</style>
--- frontend/src/components/HouseList.vue
+++ frontend/src/components/HouseList.vue
@@ -1,92 +1,98 @@
 <template>
-    <el-container style="width: 900px">
+    <el-container>
         <el-aside width="200">
             <HouseFilter v-model="filter" @onChange="reload"/>
         </el-aside>
         <el-container>
             <el-main>
                 <el-row>
-                <el-table :data="tableData">
-                    <el-table-column
-                        prop="attributes.name"
-                        label="Name"
-                        width="180"
-                    />
-                    <el-table-column
-                        prop="attributes.bedroomCount"
-                        label="Bedrooms"
-                        width="100"
-                    />
-                    <el-table-column
-                        prop="attributes.bathroomCount"
-                        label="Bathrooms"
-                        width="100"
-                    />
-                    <el-table-column
-                        prop="attributes.storeyCount"
-                        label="Storeys"
-                        width="100"
-                    />
-                    <el-table-column
-                        prop="attributes.garageCount"
-                        label="Garages"
-                        width="100"
-                    />
-                    <el-table-column
-                        prop="attributes.price"
-                        label="Price"
-                        width="100"
-                    />
-                </el-table>
-            </el-row>
+                    <el-table :data="tableData">
+                        <el-table-column
+                            prop="attributes.name"
+                            label="Name"
+                            width="180"
+                        />
+                        <el-table-column
+                            prop="attributes.bedroomCount"
+                            label="Bedrooms"
+                            width="95"
+                        />
+                        <el-table-column
+                            prop="attributes.bathroomCount"
+                            label="Bathrooms"
+                            width="95"
+                        />
+                        <el-table-column
+                            prop="attributes.storeyCount"
+                            label="Storeys"
+                            width="95"
+                        />
+                        <el-table-column
+                            prop="attributes.garageCount"
+                            label="Garages"
+                            width="95"
+                        />
+                        <el-table-column
+                            prop="attributes.price"
+                            label="Price"
+                            width="125"
+                        />
+                    </el-table>
+                </el-row>
             </el-main>
         </el-container>
     </el-container>
 </template>
 
-    <script lang="ts">
-        import { defineComponent } from 'vue'
-        import axios from 'axios'
-        import type { House } from '../models/House'
-        import qs from 'qs';
-        import HouseFilter from './HouseFilter.vue'
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    import axios from 'axios'
+    import type { House } from '../models/House'
+    import qs from 'qs';
+    import HouseFilter from './HouseFilter.vue'
 
-        interface HouseResponse {
-            data: Array<House>
-        }
+    interface HouseResponse {
+        data: Array<House>
+    }
 
-        export default defineComponent({
-            data() {
-                return {
-                    tableData: Array<House>(),
-                    filter: {
-                        name: null,
-                        bedroomCount: null,
-                        bathroomCount: null,
-                        storeyCount: null,
-                        garageCount: null,
-                        price: [0, 1000000],
-                    }
+    export default defineComponent({
+        data() {
+            return {
+                tableData: Array<House>(),
+                filter: {
+                    name: null,
+                    bedroomCount: null,
+                    bathroomCount: null,
+                    storeyCount: null,
+                    garageCount: null,
+                    price: [0, 1000000],
                 }
-            },
-            mounted() {
-                this.reload();
-            },
-            methods: {
-                reload() {
-                    const host = import.meta.env.DEV ? '' : import.meta.env.VITE_API_HOST
-                    
-                    let url = host + '/api/v1/houses';
-                    if (this.filter) {
-                        url += '?' + qs.stringify({filter: this.filter});
-                    }
-                    axios.get<HouseResponse>(url).then(response => {
-                        this.tableData = response.data.data
-                    })
-                }
-            },
-            components: {
-                HouseFilter
             }
-        })
-    </script>
+        },
+        mounted() {
+            this.reload();
+        },
+        methods: {
+            reload() {
+                const host = import.meta.env.DEV ? '' : import.meta.env.VITE_API_HOST
+                
+                let url = host + '/api/v1/houses';
+                if (this.filter) {
+                    url += '?' + qs.stringify({filter: this.filter});
+                }
+                axios.get<HouseResponse>(url).then(response => {
+                    this.tableData = response.data.data
+                })
+            }
+        },
+        components: {
+            HouseFilter
+        }
+    })
+</script>
+
+<style scoped>
+.el-container {
+    width: 975px;
+}
+</style>
